0f3da9db795cb4997a973aab014a343f0f886b17
Tharunkumar001 <tharung1411@gmail.com>
2020-12-10 07:27:25 +0530

    working on js file

--- Tip-Calculator/index.js
+++ Tip-Calculator/index.js
@@ -1,4 +1,10 @@
-function calculation(){
-    var bill = document.getElementsByName('Bill').values;
-    console.log(bill);
+
+    function calcuLation(){
+        var billamount = document.getElementById("bill").value;
+        var tipamount  = document.getElementById("tip").value;
+        var member  = document.getElementById("Members").value;
+        
+
+        
+    
 }
