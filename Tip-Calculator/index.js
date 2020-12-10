50a82b27264a067e77446d640140f9cdd686ec16
Tharunkumar001 <tharung1411@gmail.com>
2020-12-10 19:22:32 +0530

    js modification

--- Tip-Calculator/index.js
+++ Tip-Calculator/index.js
@@ -3,8 +3,15 @@
         var billamount = document.getElementById("bill").value;
         var tipamount  = document.getElementById("tip").value;
         var member  = document.getElementById("Members").value;
-        
+        var option = document.getElementById("option").value;
 
-        
+        const tips = option === "%" ? (((tipamount/100)*billamount)/member): tipamount;
+
+        const totalBill = (((billamount/member) + tips))
+
     
+
+        document.getElementById('tips').innerHTML =`Tip Amount ${tips.toPrecision(4)} \t per head`;
+        document.getElementById('total').innerHTML = `Total Amount ${totalBill.toPrecision(4)} \t per head`
+
 }
