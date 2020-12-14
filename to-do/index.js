567b819baf6224a125d1ea75717be057b6ce1f13
Tharunkumar001 <tharung1411@gmail.com>
2020-12-14 19:33:54 +0530

    login page

--- to-do/index.js
+++ to-do/index.js
@@ -1,28 +1,34 @@
 function submiting(){
-    var u = document.getElementById('user').value;
-    alert(`lets go ahead buddy ${u}`)
+ var userName = document.getElementById('user');
+
+ console.log(userName)
+ alert("ji")
 }
 
 function formHandle(){
     var user = document.createElement('input');
     user.setAttribute("type","text");
     user.setAttribute("placeholder","username");
-    document.getElementById("user").appendChild(user);
+    document.getElementById("user").appendChild(user)
+  
+
 
     var email = document.createElement("input");
     email.setAttribute("type","email");
     email.setAttribute("placeholder","Email");
     document.getElementById("email").appendChild(email);
 
+
     var password = document.createElement("input");
     password.setAttribute("type","password");
     password.setAttribute("placeholder","Password");
     document.getElementById("pass").appendChild(password);
 
+
     var button = document.createElement("input");
     button.setAttribute("type","button"); 
     button.setAttribute("value","submit"); 
-    document.getElementById("but").appendChild(button).onclick = function() {submiting()};;
+    document.getElementById("but").appendChild(button);
 
 }
 
