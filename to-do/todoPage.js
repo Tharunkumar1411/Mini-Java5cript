1ce8a5c9b5b685f2fa16f6704326bc17ccbaf93a
Tharunkumar001 <tharung1411@gmail.com>
2020-12-16 20:37:27 +0530

    model form design

--- to-do/todoPage.js
+++ to-do/todoPage.js
@@ -1,13 +1,32 @@
 function navOnclick(){
+    alert("working")
+}
+function todoPage(){
+
+    document.getElementById("nav").style.visibility="visible";
+ 
+    document.getElementById("navButton").onclick = function() {navOnclick()}
+ 
+    document.getElementById("plus").onclick = function() {plusOnclick()}
+ }
+ 
+function plusOnclick() {
+    var ModelWork = document.createElement("input");
+    document.getElementById("modelPurpose").appendChild(ModelWork);
+  
+
+    var DeadLine = document.createElement("input");
+    DeadLine.setAttribute("type","time");
+    document.getElementById("EndTime").appendChild(DeadLine);
+
+    document.getElementById("model").style.display = "block";
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick  = function() {document.getElementById("model").style.display="none",document.getElementById("modelPurpose").style.display="none"}
     
 }
 
-function todoPage(){
 
-   document.getElementById("nav").style.visibility="visible";
-
-   document.getElementById("navButton").onclick = function() {navOnclick()}
-}
 
 function todolist(){
     document.getElementById("form").style.visibility = "hidden"
