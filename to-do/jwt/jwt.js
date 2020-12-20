259b87df245c2d9d8fc4b9a12b97f3672381bed2
Tharunkumar001 <tharung1411@gmail.com>
2020-12-20 17:00:22 +0530

    jwt

--- to-do/jwt/jwt.js
+++ to-do/jwt/jwt.js
@@ -1,12 +1,19 @@
-function resultDecode(rawStr){
+function resultEncode(rawStr){
     var wordArray = CryptoJS.enc.Utf8.parse(rawStr)
     var result = CryptoJS.enc.Base64.stringify(wordArray)
     return result
 }
 
+function resultDecode(encStr){
+    var wordArrayEn = CryptoJS.enc.Base64.parse(encStr)
+    var resultEn = wordArrayEn.toString(CryptoJS.enc.Utf8)
+    return resultEn
+}
+
 function jwt(){
     console.log("ji")
     var userDecode = document.getElementById("uname").value;
-    var token = resultDecode(userDecode)
-    console.log(token)
+    var token = resultEncode(userDecode)
+    var encode = resultDecode(token)
+    console.log(token,encode)
 }
