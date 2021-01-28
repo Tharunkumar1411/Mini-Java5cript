159f4e438dde1f16e962dde30e9cdbffa5f9bced
Tharunkumar001 <tharung1411@gmail.com>
2021-01-28 16:14:25 +0530

    fetch purpose values

--- to-do/TodoPurposeBox.js
+++ to-do/TodoPurposeBox.js
@@ -5,6 +5,15 @@ function fetchPurpose(){
     var ema = decodeToken(getToken_fromLocalStorage)
     var data = {"email":ema}
     axios.post("http://localhost:3000/fetchPurpose",data).then(res => {
-        console.log(res.data)
+        console.log(res.data.purpose.length)
+       var mainContainer = document.getElementById("listCreate")
+        for (let index = 0; index < res.data.purpose.length; index++) {
+                var div = document.createElement("div");
+                div.setAttribute("id","divv")
+                div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index] +"<br><br>"
+                mainContainer.appendChild(div)
+            
+        }
+        
     })
 }
