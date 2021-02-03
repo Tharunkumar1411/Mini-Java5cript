a33c465030ada9a299615160e106b4a329a684ba
Tharunkumar001 <tharung1411@gmail.com>
2021-02-03 10:42:09 +0530

    appi

--- to-do/TodoPurposeBox.js
+++ to-do/TodoPurposeBox.js
@@ -7,10 +7,15 @@ function fetchPurpose(){
     axios.post("http://localhost:3000/fetchPurpose",data).then(res => {
         console.log(res.data.purpose.length)
        var mainContainer = document.getElementById("listCreate")
+      
         for (let index = 0; index < res.data.purpose.length; index++) {
+                var btn =  document.createElement("button");
+                btn.innerHTML = "click"
                 var div = document.createElement("div");
-                div.setAttribute("id","divv")
-                div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index] +"<br><br>"
+                div.setAttribute("id","divv") 
+
+                div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +res.data.submitDate[index]+
+                "<br>";
                 mainContainer.appendChild(div)
             
         }
