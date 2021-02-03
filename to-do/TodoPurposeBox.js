16558df0887230b7dd1718404c0ed7d5e9119983
Tharunkumar001 <tharung1411@gmail.com>
2021-02-03 13:04:16 +0530

    container design

--- to-do/TodoPurposeBox.js
+++ to-do/TodoPurposeBox.js
@@ -7,18 +7,31 @@ function fetchPurpose(){
     axios.post("http://localhost:3000/fetchPurpose",data).then(res => {
         console.log(res.data.purpose.length)
        var mainContainer = document.getElementById("listCreate")
-      
-        for (let index = 0; index < res.data.purpose.length; index++) {
-                var btn =  document.createElement("button");
-                btn.innerHTML = "click"
-                var div = document.createElement("div");
-                div.setAttribute("id","divv") 
+     
+         for (let index = 0; index < res.data.purpose.length; index++) {
+               
+        
+            var div = document.createElement("p");
+            div.setAttribute("id","divv") 
+            div.setAttribute("minlength","20");
 
-                div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +res.data.submitDate[index]+
-                "<br>";
-                mainContainer.appendChild(div)
-            
-        }
+            div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+res.data.submitDate[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
+            "<br>";
+           
+            mainContainer.appendChild(div)
+        
+    }  
+      var clearinput  =  document.createElement("input");
+      clearinput.setAttribute("id","clearinput");        
+
+      var btn = document.createElement("button");
+      btn.setAttribute("id","clear")
+      btn.setAttribute("type","submit");
+      btn.innerHTML = "clear";
+      
+      mainContainer.appendChild(clearinput)
+      mainContainer.appendChild(btn)
+       
         
     })
 }
