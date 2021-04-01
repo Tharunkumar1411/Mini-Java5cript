6a80296abff18f6e896cac8fc42dd6bc53568d0e
Tharunkumar001 <tharung1411@gmail.com>
2021-04-01 07:59:39 +0530

    COMPONENT DESIGN

--- to-do/TodoPurposeBox.js
+++ to-do/TodoPurposeBox.js
@@ -1,37 +1,52 @@
-
-
-function fetchPurpose(){
+function fetchPurpose() {
     var getToken_fromLocalStorage = localStorage.getItem("token");
     var ema = decodeToken(getToken_fromLocalStorage)
-    var data = {"email":ema}
-    axios.post("http://localhost:3000/fetchPurpose",data).then(res => {
+    var data = { "email": ema }
+    axios.post("http://localhost:3000/fetchPurpose", data).then(res => {
         console.log(res.data.purpose.length)
-       var mainContainer = document.getElementById("listCreate")
-     
-         for (let index = 0; index < res.data.purpose.length; index++) {
-               
-        
+        var mainContainer = document.getElementById("listCreate")
+
+        for (let index = 0; index < res.data.purpose.length; index++) {
+
+
             var div = document.createElement("p");
-            div.setAttribute("id","divv") 
-            div.setAttribute("minlength","20");
+            div.setAttribute("id", "divv")
+            div.setAttribute("minlength", "20");
 
-            div.innerHTML = (index+1)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+res.data.submitDate[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index]+ '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-            "<br>";
-           
+            div.innerHTML = (index + 1) + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.submitDate[index] + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + res.data.purpose[index] + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
+                "<br>";
+
             mainContainer.appendChild(div)
-        
-    }  
-      var clearinput  =  document.createElement("input");
-      clearinput.setAttribute("id","clearinput");        
 
-      var btn = document.createElement("button");
-      btn.setAttribute("id","clear")
-      btn.setAttribute("type","submit");
-      btn.innerHTML = "clear";
-      
-      mainContainer.appendChild(clearinput)
-      mainContainer.appendChild(btn)
-       
-        
+        }
+
+        function clearTodo() {
+            const noOfremovedTodo = document.getElementById('clearinput').value;
+            var dataforRemoveTodo = {
+                Email: decodeToken(localStorage.getItem("token")),
+                index: noOfremovedTodo
+            }
+
+
+
+            axios.put("http://localhost:3000/removeTodo", dataforRemoveTodo).then(response => {
+                console.log(response)
+            })
+        }
+
+        var clearinput = document.createElement("input");
+        clearinput.setAttribute("id", "clearinput");
+        clearinput.setAttribute("type", "number");
+
+        var btn = document.createElement("button");
+        btn.setAttribute("id", "clear")
+        btn.setAttribute("type", "submit");
+        btn.innerHTML = "clear";
+
+        mainContainer.appendChild(clearinput)
+        mainContainer.appendChild(btn)
+
+        document.getElementById("clear").onclick = function() { clearTodo() }
+
     })
 }
